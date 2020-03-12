4e39ca50d078cd3937cc35583e48be52cb40b46d
Atlasfreak <atlasfreak@gmx.de>
2020-03-12 16:03:30 +0100

    added Vplan database

--- vertretungsplan_webapp/models.py
+++ vertretungsplan_webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Vplan_schueler(models.Model):
+    pos = models.IntegerField(verbose_name="Unterichtsstunde")
+    fach = models.CharField(max_length=100, verbose_name="Fach")
+    raum = models.CharField(max_length=100, verbose_name="Raum")
+    klasse = models.CharField(max_length=100, verbose_name="Klasse")
+    info = models.CharField(max_length=100, verbose_name="Informationen")
+    art = models.CharField(max_length=100, verbose_name="Art")   
